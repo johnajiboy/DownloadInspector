2e62bc8a52bf3d920e80a216e0c7141de387d44b
OnyeEbube <okontaonyeka@gmail.com>
2023-06-09 10:47:54 +0100

    Removing some updated and adding some

--- webapp/app.py
+++ webapp/app.py
@@ -1,45 +1,23 @@
-from flask import Flask, request, jsonify, render_template, redirect
+from flask import Flask, request, render_template
 from flask_cors import CORS
-from functions import check_malicious, create_users_table, create_user, check_login_info
+from functions import check_malicious
 
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/signup', methods=['POST', 'GET'])
-def signup():
-    if request.method == 'POST':
-        name = request.form['name']
-        email = request.form['email']
-        password = request.form['password']
-        create_users_table()
-        create_user(name, email, password)
-        return redirect('/login')
-    return render_template('signup.html')
-
-@app.route('/login', methods=['POST', 'GET'])
-def login():
-    if request.method == 'POST':
-        name = request.form['name']
-        email = request.form['email']
-        password = request.form['password']
-        if check_login_info(name, password) == True:
-            return redirect('/')
-        else:
-            return redirect('/login')
-    return render_template('login.html')
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
-   if request.method == 'POST':
-      data = request.form
-      download_link = data.get('link')
-      is_malicious = check_malicious(download_link)
-      return render_template('result.html', is_malicious=is_malicious, link=download_link)
-   else:
-       return render_template('index.html')
+    if request.method == 'POST':
+        data = request.form
+        download_link = data.get('link')
+        is_malicious = check_malicious(download_link)
+        return render_template('result.html', is_malicious=is_malicious, link=download_link)
+    else:
+        return render_template('index.html')
 
 def run_app():
     app.run(port=5001, debug=True)
+
 if __name__ == '__main__':
     run_app()
--- webapp/functions.py
+++ webapp/functions.py
@@ -21,7 +21,6 @@ def check_malicious(download_link):
         modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
     )
     """
-
     cursor.execute(create_table_query)
     db_connection.commit()
 
@@ -41,25 +40,19 @@ def check_malicious(download_link):
         response = perform_scanning(download_link)
         # Store the response in the database
         insert_result_query = "INSERT INTO scan_results (link, result) VALUES (%s, %s)"
-        cursor.execute(insert_result_query, (download_link, response))
+        cursor.execute(insert_result_query, (download_link, response,))
         db_connection.commit()
 
         cursor.close()
         db_connection.close()
 
         return response
-
-#def has_link_content_changed(download_link, stored_response):
- #   current_response = perform_scanning(download_link)
-
-  #  if current_response != stored_response:
-   #     return True
-    #else:
-     #   return False
-
+    
 def perform_scanning(download_link):
     scan_url = 'https://www.filescan.io/api/scan/url'
-    payload = {'url': download_link}
+    payload = {
+        'url': download_link
+        }
     headers = {
         "x-apikey": "cggo-adW_tAzg1KrML9eXpqXy4VVca8hmuxEBAgj",
         "content-type": "application/x-www-form-urlencoded"
@@ -67,13 +60,14 @@ def perform_scanning(download_link):
 
     response = requests.post(scan_url, data=payload, headers=headers)
 
+
     if response.status_code == 200:
         data = response.json()
         flow_id = data['flow_id']
         id = f"https://filescan.io/api/scan/{flow_id}/report?filter=general&filter=finalVerdict&sorting=string&other=emulationGraph"
         headers = {
-            "x-apikey": "cggo-adW_tAzg1KrML9eXpqXy4VVca8hmuxEBAgj",
-            "Content-Type": "application/json"
+        "x-apikey": "cggo-adW_tAzg1KrML9eXpqXy4VVca8hmuxEBAgj",
+        "Content-Type": "application/json"
         }
 
         while True:
@@ -90,59 +84,3 @@ def perform_scanning(download_link):
                     return is_malicious
                 if not data['allFinished']:
                     time.sleep(5)
-
-def create_users_table():
-    db_connection = mysql.connector.connect(
-        host = "localhost",
-        user = "root",
-        password = "fumCHI@02%w#",
-        database = "download_inspector"
-    )
-    cursor = db_connection.cursor()
-    create_table_query = """
-    CREATE TABLE IF NOT EXISTS users (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        name TEXT NOT NULL,
-        email TEXT NOT NULL,
-        password TEXT NOT NULL
-    )"""
-    cursor.execute(create_table_query)
-    db_connection.commit()
-    db_connection.close()
-
-def create_user(name, email, password):
-    db_connection = mysql.connector.connect(
-        user = "root",
-        host = "localhost",
-        password = "fumCHI@02%w#",
-        database = "download_inspector"
-    )
-    cursor = db_connection.cursor()
-    insert_users_query = """
-    INSERT INTO users (
-        name, email, password)
-        VALUES (%s, %s, %s)
-    """
-    cursor.execute(insert_users_query, (name, email, password))
-    db_connection.commit()
-    db_connection.close()
-    
-def check_login_info(name, password):
-    db_connector = mysql.connector.connect(
-        user = "root",
-        host = "localhost",
-        password = "fumCHI@02%w#",
-        database = "download_inspector"    
-    )
-    cursor = db_connector.cursor()
-    check_login_query = """
-    SELECT * FROM users WHERE name = %s AND password = %s
-    """
-    cursor.execute(check_login_query, (name, password,))
-    user = cursor.fetchone()
-
-    if user:
-        return True
-    else:
-        return False
-    
